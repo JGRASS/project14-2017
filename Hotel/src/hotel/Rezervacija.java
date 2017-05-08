19c21cc044186345f4a62dc5dabc978b289ac104
jelicastanojevic <jelicastanojevicc@gmail.com>
2017-05-08 19:56:21 +0200

    Napravljen paket hotel sa klasama Soba i Rezervacija

---
+++ Hotel/src/hotel/Rezervacija.java
@@ -0,0 +1,14 @@
+package hotel;
+
+import java.util.GregorianCalendar;
+
+public class Rezervacija {
+	int idRezervacije;
+	int idSobe;
+	String imeGosta;
+	String prezimeGosta;
+	GregorianCalendar datumOd;
+	GregorianCalendar datumDo;
+	
+	
+}
---
+++ Hotel/src/hotel/Soba.java
@@ -0,0 +1,50 @@
+package hotel;
+
+public class Soba {
+	int idSobe;
+	int brojKreveta;
+	int cena;
+	int sprat;
+	boolean terasa;
+
+	public int getIdSobe() {
+		return idSobe;
+	}
+
+	public void setIdSobe(int idSobe) {
+		this.idSobe = idSobe;
+	}
+
+	public int getBrojKreveta() {
+		return brojKreveta;
+	}
+
+	public void setBrojKreveta(int brojKreveta) {
+		this.brojKreveta = brojKreveta;
+	}
+
+	public int getCena() {
+		return cena;
+	}
+
+	public void setCena(int cena) {
+		this.cena = cena;
+	}
+
+	public int getSprat() {
+		return sprat;
+	}
+
+	public void setSprat(int sprat) {
+		this.sprat = sprat;
+	}
+
+	public boolean isTerasa() {
+		return terasa;
+	}
+
+	public void setTerasa(boolean terasa) {
+		this.terasa = terasa;
+	}
+
+}
