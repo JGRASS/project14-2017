2440a41c31aeaca99db777ca880d137eef48037f
veljkoterzic <veljkot@gmail.com>
2017-05-08 23:45:38 +0200

    izmenjen konektor

--- Hotel/src/hotel/baza/DBConnector.java
+++ Hotel/src/hotel/baza/DBConnector.java
@@ -1,19 +1,30 @@
 package hotel.baza;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author veljko
  *
  */
 public class DBConnector {
+
 	Connection con = null;
 
-	String url = "jdbc:mysql://localhost:3306/HotelReservations";
+	String driver = "com.mysql.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/Hotel";
 	String user = "HotelEmployee";
 	String pass = "asus";
 
 	public Connection connect() throws SQLException {
-		con = DriverManager.getConnection(url, user, pass);
+		try {
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url, user, pass);
+			return con;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		return con;
 	}
 }
