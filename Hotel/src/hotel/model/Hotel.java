1bcb82bfbd113c648b273e7b7ec72185574a8c0a
veljkoterzic <veljkot@gmail.com>
2017-05-09 01:40:30 +0200

    dodata probna verzija za izlastajSveSobe

--- Hotel/src/hotel/model/Hotel.java
+++ Hotel/src/hotel/model/Hotel.java
@@ -3,25 +3,42 @@
  */
 package hotel.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
+import hotel.Rezervacija;
+import hotel.Soba;
+import hotel.baza.DBConnector;
 import hotel.interfejs.HotelInterfejs;
 
 /**
  * @author jelica&veljko
  *
  */
+
 public class Hotel implements HotelInterfejs {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hotel.interfejs.HotelInterfejs#rezervisi(int)
 	 */
+
+	public static DBConnector connector = new DBConnector();
+
 	@Override
 	public void rezervisi(int idSobe) {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hotel.interfejs.HotelInterfejs#otkazi(int)
 	 */
 	@Override
@@ -29,23 +46,30 @@ public class Hotel implements HotelInterfejs {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hotel.interfejs.HotelInterfejs#vratiSveSobe()
 	 */
 	@Override
 	public void vratiSveSobe() {
-
+		
 	}
 
-	/* (non-Javadoc)
-	 * @see hotel.interfejs.HotelInterfejs#izlistaj(java.util.GregorianCalendar, java.util.GregorianCalendar, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see hotel.interfejs.HotelInterfejs#izlistaj(java.util.GregorianCalendar,
+	 * java.util.GregorianCalendar, int)
 	 */
 	@Override
 	public void izlistaj(GregorianCalendar datumOd, GregorianCalendar datumDo, int brKreveta) {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hotel.interfejs.HotelInterfejs#vratiSaTerasom()
 	 */
 	@Override
@@ -53,7 +77,9 @@ public class Hotel implements HotelInterfejs {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hotel.interfejs.HotelInterfejs#vratiSaKrevetom(int)
 	 */
 	@Override
@@ -61,12 +87,19 @@ public class Hotel implements HotelInterfejs {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see hotel.interfejs.HotelInterfejs#vratiSprat(int)
 	 */
 	@Override
 	public void vratiSprat(int sprat) {
 
+	}
+	
+	public static void main(String[] args) {
+		Hotel h=new Hotel();
+		h.vratiSveSobe();
 	}
 
 }
